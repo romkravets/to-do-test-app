0770ed8000c3c54fb99d3f7e662e6aa8270e2a79
romkravets <romann.kravets@gmail.com>
2019-06-26 12:17:19 +0300

    Jsx to-do

--- src/components/task-list/TaskList.js
+++ src/components/task-list/TaskList.js
@@ -18,6 +18,32 @@ export class TaskList extends React.Component {
             />
             <button className="task-list__btn">Add</button>
          </form>
+         <div className="list">
+            <ul className="task-list__content">
+            <li>
+                  <span className="list-item__checkbox">
+                  <input className="list-item__native-input"
+                        type="checkbox"
+                        id=""/>
+                  <label className="list-item__label"></label>
+                  </span>
+                  <span className="list-item__title">
+                  </span>
+                  <input className="list-item__input"
+                  value=''/>
+                  <button className="list-item__del">X</button>
+            </li>
+            </ul>
+         </div>
+         <div className="footer">
+            <div className="footer__counter">
+               1
+            </div>
+            <div className="footer__filters">
+               2
+            </div>
+            <div className="footer__control"></div>
+         </div>
          </div>
       )
    }
